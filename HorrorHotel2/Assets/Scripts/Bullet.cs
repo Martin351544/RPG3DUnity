12cddb09bee5fb4b50ed0ccdedeb88a80082b41c
Martin <Martin.weiss2410@gmail.com>
2025-10-01 07:32:27 +0700

    guns

---
+++ HorrorHotel2/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("hit" + collision.gameObject.name + "!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ HorrorHotel2/Assets/Scripts/Weapon.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f;
+    public float shootingInterval = 0.3f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+
+        StartCoroutine(DestroyBullet(bullet, bulletPrefabLifeTime));
+
+    }
+
+    public IEnumerator DestroyBullet(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+}
